a86bf9a0ca0fe66e04ef4a44ca00a4f48a41254c
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-09-15 18:11:02 +0530

    MirageConfig: mock CRUD endpoints for post

--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,31 @@
 export default function() {
   this.namespace = 'api/v1/';
-  this.get('posts');
+  this.get('posts', function(){
+    return [{
+      title: 'Post 1',
+      body: "Post 1 for testing",
+      tag_list: ["test"]
+    }, {
+      title: 'Post 2',
+      body: "Post 2 for testing",
+      tag_list: ["test"]
+    }]
+  });
+  this.get('posts/post-1', function(){
+    return {
+      title: 'Post 1',
+      body: "Post 1 for testing",
+      tag_list: ["test"]
+    }
+  });
+  this.put('posts/post-1', function(){
+    return {
+      title: 'Post 1',
+      body: "Changed the body content",
+      tag_list: ["test"]
+    }
+  });
+  this.delete('posts/post-1', function(){
+    return true;
+  });
 }
