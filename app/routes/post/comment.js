871cab29df5f28c6628bed17369917397ab77061
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-09-16 13:59:39 +0530

    PostRoute: redirect to post after saving new comment

--- app/routes/post/comment.js
+++ app/routes/post/comment.js
@@ -3,19 +3,22 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Route.extend(AuthenticatedRouteMixin, {
   post_id: '',
+  post_slug: '',
   model(params) {
     this.set('post_id', params.post_id)
+    this.set('post_slug', params.post_slug)
     return this.store.createRecord('post');
   },
   actions: {
     saveComment(comment) {
       let post_id = this.get('post_id');
+      let post_slug = this.get('post_slug');
       comment.set('post_id', post_id);
       comment.save().then(() => {
         this.get('notifications').success('Saved successfully!', {
           autoClear: true
         });
-        this.transitionTo('post')
+        this.transitionTo('post.show',post_slug)
       });
     }
   }
