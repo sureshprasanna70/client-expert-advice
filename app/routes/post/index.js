d8b1e0b6048a587aa7b2619bbf7d6f20c6d3bed0
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-09-17 20:59:12 +0530

    PostRoute: handles queryParams for tag search

--- app/routes/post/index.js
+++ app/routes/post/index.js
@@ -5,9 +5,26 @@ import {
 
 export default Route.extend({
   infinity: service(),
-  model() {
+  setupController: function (controller, model) {
+    this._super(...arguments);
+    let searchedTag = model.extraParams.tag
+    if (searchedTag) {
+      controller.set('tagSearch', true);
+      controller.set('searchedTag', searchedTag);
+    } else {
+      controller.set('tagSearch', false)
+    }
+  },
+  queryParams: {
+    tag: {
+      refreshModel: true
+    }
+  },
+  model(params) {
+    const tag = params.tag;
     return this.infinity.model("post", {
       perPage: 5,
+      tag: tag,
       totalPagesParam: "meta.pages",
       countParam: "meta.total"
     });
